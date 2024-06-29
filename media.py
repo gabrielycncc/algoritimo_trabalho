4badc3ed6aca7cceeaae044560dc2ef9180d392b
Manozrr <renanyea32@gmail.com>
2024-06-29 15:49:35 -0300

    2 Save commit

--- media.py
+++ media.py
@@ -1,15 +1,14 @@
-nome = float(input('Escreva o Nome do aluno: '))
+nome  = input('Escreva o Nome do aluno: ')
 nota1 = float(input('Primeira nota: '))
 nota2 = float(input('Segunda nota: '))
 nota3 = float(input('Terceira nota: '))
 nota4 = float(input('Quarta nota: '))
-resultado = (nota1 + nota2) / 2
-print(f'Nome: [nome]')
-print(f'Resultado:'[resultado])
+resultado = (nota1 + nota2 + nota3 + nota4) / 4
+print('Nome:',nome)
+print('A média final foi: %.1f' %resultado)
 
 if resultado >= 5:
     print('Parabéns Você foi aprovado!')
 
-
-elif resultado <= 4:
+else:
     print ('Você foi reprovado!')
